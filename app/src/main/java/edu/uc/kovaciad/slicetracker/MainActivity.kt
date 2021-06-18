a382709f2d43f7a5d90c6fcc395fe71b14dd7145
bojesphob <schmidjp@ucmail.uc.edu>
2021-06-18 14:11:35 -0400

    DATABASE info added

--- app/src/main/java/edu/uc/kovaciad/slicetracker/MainActivity.kt
+++ app/src/main/java/edu/uc/kovaciad/slicetracker/MainActivity.kt
@@ -1,18 +1,66 @@
 package edu.uc.kovaciad.slicetracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import edu.uc.kovaciad.slicetracker.ui.main.MainFragment
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
 
 class MainActivity : AppCompatActivity() {
 
+    //Connection information to my database
+    //Schema IT3048C
+    val ip = "173.248.174.34"
+    val port = "1533"
+    val Classes = "net.sourceforge.jtds.jdbc.Driver"
+    val database = "personaldatabase"
+    val username = "webapplication"
+    val password = "I@mtheW@lrus1"
+    val url = "jdbc:jtds:sqlserver://$ip:$port/$database"
+
+    val connection : Connection = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+//        setContentView(R.layout.main_activity)
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
+//
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        ActivityCompat.requestPermissions(
+//            this,
+//            arrayOf(Manifest.permission.INTERNET),
+//            PackageManager.PERMISSION_GRANTED
+//        )
+//        textView = findViewById<View>(R.id.textView)
+//
+//        val policy = ThreadPolicy.Builder().permitAll().build()
+//        StrictMode.setThreadPolicy(policy)
+//        try {
+//            Class.forName(Classes)
+//            connection = DriverManager.getConnection(url, username, password)
+//            textView.setText("SUCCESS")
+//        } catch (e: ClassNotFoundException) {
+//            e.printStackTrace()
+//            textView.setText("ERROR")
+//        } catch (e: SQLException) {
+//            e.printStackTrace()
+//            textView.setText("FAILURE")
+//        }
     }
+
+
 }
