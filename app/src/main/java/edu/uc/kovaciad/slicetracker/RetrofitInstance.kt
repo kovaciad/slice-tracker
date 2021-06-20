022e37591ab09fb5305a49b1493cada649de7bea
Aidan Kovacic <kovaciad@mail.uc.edu>
2021-06-20 00:56:22 -0400

    Added Retrofit

---
+++ app/src/main/java/edu/uc/kovaciad/slicetracker/RetrofitInstance.kt
@@ -0,0 +1,20 @@
+package edu.uc.kovaciad.slicetracker
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private var retrofit: Retrofit? = null
+    private val BASE_URL = "127.0.0" // TODO: Replace with our webapp (Sprint 2)
+
+    val retrofitInstance: Retrofit?
+        get() {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+}
