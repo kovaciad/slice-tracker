b589cfea449bb986ee9f7d8570e0a402a3ef8af0
Aidan Kovacic <kovaciad@mail.uc.edu>
2021-07-17 19:00:39 -0400

    Removed accidental references to RoomDB

--- app/src/test/java/edu/uc/kovaciad/slicetracker/DataUnitTest.kt
+++ app/src/test/java/edu/uc/kovaciad/slicetracker/DataUnitTest.kt
@@ -1,8 +1,5 @@
 package edu.uc.kovaciad.slicetracker
 
-import android.util.Log
-import androidx.room.Room
-import edu.uc.kovaciad.slicetracker.dto.SliceFile
 import junit.framework.Assert.assertEquals
 
 import org.junit.Test
