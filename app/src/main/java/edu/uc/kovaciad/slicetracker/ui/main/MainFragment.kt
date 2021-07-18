754093671330ea571da7fc4946b431c3f49e62b9
Aidan Kovacic <kovaciad@mail.uc.edu>
2021-07-17 22:46:48 -0400

    Added UI Test for saving

--- app/src/main/java/edu/uc/kovaciad/slicetracker/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/kovaciad/slicetracker/ui/main/MainFragment.kt
@@ -2,8 +2,11 @@ package edu.uc.kovaciad.slicetracker.ui.main
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import edu.uc.kovaciad.slicetracker.R
+import edu.uc.kovaciad.slicetracker.dto.Printer
 
 
 class MainFragment : Fragment() {
@@ -21,5 +24,17 @@ class MainFragment : Fragment() {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // Synthetics aren't working at the moment and neither is data binding
+        val btnSaveTest = getView()?.findViewById<Button>(R.id.btnSaveTest)
+
+        btnSaveTest?.setOnClickListener {
+            viewModel.save(Printer("Ender 3 Pro", "asduy324", "kjdfsh2", ), "printers")
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 
 }
