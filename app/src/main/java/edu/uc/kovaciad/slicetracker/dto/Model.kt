8ebd44cc1cee94c9ead9c3f97b765b0c5b7c3811
Ruairi Blake <Rblake0601@gmail.com>
2021-07-25 13:51:30 -0400

    updates param name to match the rest of the params

--- app/src/main/java/edu/uc/kovaciad/slicetracker/dto/Model.kt
+++ app/src/main/java/edu/uc/kovaciad/slicetracker/dto/Model.kt
@@ -4,12 +4,12 @@ package edu.uc.kovaciad.slicetracker.dto
  * @param id: DB ID for dev use
  * @param modelName: User Inputted Name
  * @param artistId: Artist ID from Artist table
- * @param url: model URL
+ * @param modelURL: model URL
  */
 data class Model (
             var modelName: String = "",
             var artistId: String = "",
-            var url: String? = null,
+            var modelURL: String? = null,
             override var id: String = ""): IData {
 
     override fun toString(): String {
