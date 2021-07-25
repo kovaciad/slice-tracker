969c6dcecdbb35fe76406c80689f1dda71cd2981
Ruairi Blake <Rblake0601@gmail.com>
2021-07-25 13:44:36 -0400

    adds param description

--- app/src/main/java/edu/uc/kovaciad/slicetracker/dto/Brand.kt
+++ app/src/main/java/edu/uc/kovaciad/slicetracker/dto/Brand.kt
@@ -3,6 +3,7 @@ package edu.uc.kovaciad.slicetracker.dto
 /**
  * @param id: DB ID for dev use
  * @param brandName: User Inputted Name
+ * @param brandURL: URL string of brand
  */
 data class Brand(
             var brandName: String = "",
