1aab03300faca4c5d02fb54bccb9b700380734e9
Aidan Kovacic <kovaciad@mail.uc.edu>
2021-08-06 16:33:59 -0400

    Added current city view using LocationData

--- app/src/main/java/edu/uc/kovaciad/slicetracker/ui/main/OverviewFragment.kt
+++ app/src/main/java/edu/uc/kovaciad/slicetracker/ui/main/OverviewFragment.kt
@@ -1,13 +1,16 @@
 package edu.uc.kovaciad.slicetracker.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,13 +19,12 @@ import edu.uc.kovaciad.slicetracker.dto.SliceFile
 import kotlin.math.roundToLong
 import androidx.lifecycle.Observer
 
-public var currentCity = ""
-lateinit var applicationViewModel: ApplicationViewModel
-
 class OverviewFragment : SuperFragment() {
 
     private var totalPrintTime: Double = 0.0
     private var totalMats: Double = 0.0
+    private lateinit var applicationViewModel: ApplicationViewModel
+    private val LOC_REQUEST_CODE = 2000
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,17 +33,34 @@ class OverviewFragment : SuperFragment() {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.overview_fragment, container, false)
     }
-    private fun requestLocationUpdates() {
 
-        applicationViewModel.getLocationLiveData().observe(this, Observer {
-            var latitudeValue: Double = it.latitude.toDouble()
-            var longitudeValue: Double = it.longitude.toDouble()
+    private fun prepLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            requestLocationUpdates()
+        } else {
+            val permissionRequest = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION)
+            requestPermissions(permissionRequest, LOC_REQUEST_CODE)
+        }
+    }
+
+    private fun requestLocationUpdates() {
+        val cityView = requireView().findViewById<TextView>(R.id.cityView)
+        val res = resources
+        applicationViewModel = ViewModelProvider(this).get(ApplicationViewModel::class.java)
+        applicationViewModel.getLocationLiveData().observe(viewLifecycleOwner, {
+            val latitudeValue: Double = it.latitude.toDouble()
+            val longitudeValue: Double = it.longitude.toDouble()
             val geocoder = Geocoder(context)
             val list = geocoder.getFromLocation(latitudeValue, longitudeValue, 1)
-
+            var currentCity = ""
             if (list[0].locality != null) {
                 currentCity = list[0].locality
+
             }
+
+            val mText = String.format(res.getString(R.string.currentCity), currentCity)
+            cityView.text = mText
         })
     }
 
@@ -55,6 +74,8 @@ class OverviewFragment : SuperFragment() {
         This observer is a workaround for a bug where the value of the LiveData would be null on start
         It is not elegant, but it allows this to work as it waits for a value.
          */
+
+        prepLocationUpdates()
         viewModel.sliceFiles.observe(viewLifecycleOwner, {
                 sliceFiles ->
                     slices = sliceFiles
@@ -80,6 +101,28 @@ class OverviewFragment : SuperFragment() {
                 actTotalMaterials?.text = mText
         })
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            LOC_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==  PackageManager.PERMISSION_GRANTED) {
+                    requestLocationUpdates()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Unable to update location without permission",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            } else -> {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
     }
 
     inner class SliceAdapter(private val slices: ArrayList<SliceFile>, private val itemLayout: Int) : RecyclerView.Adapter<OverviewFragment.SliceViewHolder>() {
